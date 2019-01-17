b2a71f4b2313e50414ffc87ad1251afd3e47c778
Réda Ouaich <reda.ouaich@hetic.net>
2019-01-17 11:14:22 +0100

    implement react-video

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,14 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
+import VideoCover from 'react-video-cover';
 // import HomeStyle from '../../assets/style/Home.scss';
 
-const Home = () => (
-  <Fragment>
-    <video src="../../assets/video/final_wolves.mp4"></video>
-    <h3>
-      Pour un meilleur confort de lecture, veuillez porter un casque
-    </h3>
-  </Fragment>
-)
+class Home extends Component {
+  render() {
+      const videoOptions = {
+        src: '../../src/assets/video/final_wolves.mp4',
+        ref: videoRef => {
+          this.videoRef = videoRef;
+        },
+        onClick: () => {
+          if (this.videoRef && this.videoRef.paused) {
+            this.videoRef.play();
+          } else if (this.videoRef) {
+            this.videoRef.pause();
+          }
+        },
+        title: 'click to play/pause',
+      };
+      return ( 
+      <div style = {
+          {
+            width: '100%',
+            height: '100%',
+            overflow: 'hidden'
+          }
+        } >
+        <VideoCover videoOptions = {
+          videoOptions
+        }/>
+        </div>
+      );
+    }
+  }
 
 
 export default Home;
