c86a7d51423881a3cf99e420b8a7276910163971
metayerromain <romain.metayer@hetic.net>
2019-01-18 10:57:03 +0100

    video ok

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,38 +1,10 @@
 import React, { Component } from 'react';
-import VideoCover from 'react-video-cover';
-// import HomeStyle from '../../assets/style/Home.scss';
 
-class Home extends Component {
-  render() {
-      const videoOptions = {
-        src: '../../src/assets/video/final_wolves.mp4',
-        ref: videoRef => {
-          this.videoRef = videoRef;
-        },
-        onClick: () => {
-          if (this.videoRef && this.videoRef.paused) {
-            this.videoRef.play();
-          } else if (this.videoRef) {
-            this.videoRef.pause();
-          }
-        },
-        title: 'click to play/pause',
-      };
-      return ( 
-      <div style = {
-          {
-            width: '100%',
-            height: '100%',
-            overflow: 'hidden'
-          }
-        } >
-        <VideoCover videoOptions = {
-          videoOptions
-        }/>
-        </div>
-      );
-    }
-  }
-
+const Home = (props) => (
+  
+    <video src="https://clips.vorwaerts-gmbh.de/VfE_html5.mp4" type="video/mp4" className="anim" onClick={props.clickVideo} autoPlay>
+    </video>
+  
+);
 
 export default Home;
--- src/views/Home/HomeContainer.js
+++ src/views/Home/HomeContainer.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
 import Home from './Home';
+import ReactDOM from 'react-dom';
 
 class HomeContainer extends Component {
 
+  state={
+    videoOptions : { 
+      src: '../../src/assets/video/final_wolves.mp4',
+      title: 'click to play/pause',
+    },
+    paused : false
+  }
+
+  clickVideo = (e) => {
+    if(!this.state.paused){
+      e.target.pause();
+      this.setState({
+        paused : true
+      })
+    } else{
+      e.target.play();
+      this.setState({
+        paused : false
+      })
+    }
+  }
+
+  render(){
+    console.log(this.state.videoOptions)
+    return ( 
+      <Home 
+        videoOptions = {this.state.videoOptions}
+        clickVideo = {this.clickVideo}
+      />
+    );
+  }
 }
 
 export default HomeContainer;
