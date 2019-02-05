86ff1420325e8834b6772654aebbb469ac1da761
Réda Ouaich <reda.ouaich@hetic.net>
2019-02-05 14:52:02 +0100

    Lexique init & merging dev

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
 import React, { Component } from 'react';
 import './assets/style/Reset.scss'
-import ReactDOM from 'react-dom';
-import {TweenLite} from "gsap/TweenMax";
-import * as ScrollMagic from 'scrollmagic'
-import Home from './views/Home/HomeContainer';
-import Load from './views/Loader/LoaderContainer';
-import Choice from './views/Choice/ChoiceContainer';
+// import ReactDOM from 'react-dom';
+// import * as ScrollMagic from 'scrollmagic'
+// import Home from './views/Home/HomeContainer';
+// import Load from './views/Loader/LoaderContainer';
+// import Choice from './views/Choice/ChoiceContainer';
+import Lexique from './views/Lexique/LexiqueContainer';
 // import Test from './views/Test';
 
 import './App.css';
 
 class App extends Component {
 
-  componentDidMount(){
+  // componentDidMount(){
     // var anim = ReactDOM.findDOMNode(this).querySelector('.anim');
     // this.myTween = TweenLite.fromTo(anim, 1, { y: 100}, {y:0});
 
-	var controller = new ScrollMagic.Controller();
+	// var controller = new ScrollMagic.Controller();
 
-		var scene = new ScrollMagic.Scene({
-              triggerElement: ReactDOM.findDOMNode(this).querySelector('.section'),
-              triggerHook: .5 
-						})
-						.setTween("#animate1", 0.5, {backgroundColor: "green", scale: 2.5}) 
-						.addIndicators() 
-            .addTo(controller);
+	// 	var scene = new ScrollMagic.Scene({
+  //             triggerElement: ReactDOM.findDOMNode(this).querySelector('.section'),
+  //             triggerHook: .5 
+	// 					})
+	// 					.setTween("#animate1", 0.5, {backgroundColor: "green", scale: 2.5}) 
+	// 					.addIndicators() 
+  //           .addTo(controller);
             
-  }
+  // }
 
   render() {
     return (
       <div>
-        {/* <Test /> */}
-        <Load />
-        <Home /> 
-        <Choice />
+          {/* <Test /> */}
+          {/* <Load />
+          <Home /> 
+          <Choice /> */}
+        {/* Here show the lexique only if the questionnary is done */}
+         <Lexique />
         {/* <Problem/> */}
       </div>
     );
---
+++ src/views/Lexique/Lexique.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Lexique = (props) => (
+  
+    <div>
+      <div className="map1"></div>
+      <i class="fas fa-plus-circle"></i>
+    </div>
+  
+);
+
+export default Lexique;
