e94c8f8943227f99f8ae342ad491516d7e822871
Réda Ouaich <reda.ouaich@hetic.net>
2019-02-06 09:26:16 +0100

    Adding map

--- src/views/Lexique/Lexique.js
+++ src/views/Lexique/Lexique.js
@@ -4,7 +4,7 @@ const Lexique = (props) => (
   
     <div>
       <div className="map1"></div>
-      <i class="fas fa-plus-circle"></i>
+      <i className="fas fa-plus-circle"></i>
     </div>
   
 );
---
+++ src/views/Lexique/Words/Loutre.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Loutre = (props) => (
+
+  <div className="bg-modal">
+    <h2>
+      Le Savais-tu ?
+    </h2>
+    <p>
+      Pour rester proche l’une de l’autre quand elles entament leur sommeil à la surface de l’eau, 
+      les loutres de mer se tiendraient par la patte pour ne pas partir à la dérive et se perdre de vue. 
+      Un tableau attendrissant qui en fera fondre plus d’un !
+    </p>
+  </div>
+
+);
+
+export default Loutre;
