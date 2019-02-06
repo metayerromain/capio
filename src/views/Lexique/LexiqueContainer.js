14827e39ed108996c47dd016d5d6cc2a0ee12aac
Réda Ouaich <reda.ouaich@hetic.net>
2019-02-06 18:15:57 +0100

    Adding data, still need to add content

--- src/views/Lexique/LexiqueContainer.js
+++ src/views/Lexique/LexiqueContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 // import Lexique from './Lexique';
 import LexiqueModal from '../LexiqueModal'
 import '../../assets/style/Lexique.scss';
@@ -7,60 +7,69 @@ class LexiqueContainer extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { 
-      show: false 
-    };  
+    this.state = {
+      show: false,
+      lexique: null
+    };
+    this.datas = [{
+        key: 1,
+        title: "La reproduction recreative des chevres",
+        content: "La chevres des chevres des chevres"
+      },
+      {
+        key: 2,
+        title: "Le saviez-vous ?",
+        content: "Pendant longtemps, partout en Europe, le 2 février était un jour de fête païenne : le Chant de l'Ours. Cette date correspondait à la sortie de tanière du plantigrade, et les européens fêtaient la reprise de ses activités. "
+      },
+      {
+        key: 3,
+        title: "Le saviez-vous ?",
+        content: "Le wapiti a longtemps été considéré comme la plus grande des sous-espèces de cerf élaphe mais les analyses génétiques semblent montrer que le wapiti est plus proche du cerf sika."
+      },
+      {
+        key: 4,
+        title: "Le saviez-vous ?",
+        content: "Chevres"
+      }, 
+      {
+        key: 5,
+        title: "Le saviez-vous ?",
+        content: "Chevres"
+      }
+    ];
   }
 
-  data = {
-    title: {
-      "title1": "La reproduction recreative des chevres",
-      "title2": "Le fromage de chevre"
-    },
-    content: {
-      "content1": "this is content",
-      "content2": "La reproduction recreative des loups en procreation cognitive"
-    }
-  }
-
-  showModal = () => {
-    this.setState({ show: true });
-    return this.modalContent();
+  showModal = (event, id) => {
+    this.setState({ show: true, lexique : this.datas[ id ] });
   };
 
   hideModal = () => {
     this.setState({ show: false });
   };
 
-  modalContent = () => {
-    console.log(this.state.show);
-  }
-
   render() {
     return (
       <div>
+        <div className="map"></div>
         <div className="lexique-container">
           {
-          this.state.show ?
-          <LexiqueModal
-            lexiqueObject = {
-              [
-                {
-                  title: "La reproduction recreative des chevres",
-                  content: "La reproduction recreative des chevres des chevres des chevres"
-                },
-                {
-                  title: "La reproduction recreative des chevres",
-                  content: "La reproduction recreative des chevres des chevres des chevres"
-                }
-              ]
-            }/> : ''
+            this.state.show ?
+            <Fragment>
+              <div className="bg-black"></div>
+              <LexiqueModal
+                lexique = { this.state.lexique }
+              />
+              <div onClick={() => this.hideModal()} className="close"></div>
+            </Fragment> : ''
           }
         </div>
-        <div className="map1"></div>
-        <div id="icon1" onClick={() => this.showModal()} className="lexique-icon"></div>
-        <div id="icon2" onClick={() => this.showModal()} className="lexique-icon"></div>
-        <div id="icon3" onClick={() => this.showModal()} className="lexique-icon"></div>
+
+        }
+        <div id="icon1" data-id="0" onClick={() => this.showModal(null, 0)} className="lexique-icon"></div>
+        <div id="icon2" data-id="1" onClick={() => this.showModal(null, 1)} className="lexique-icon"></div>
+        <div id="icon3" data-id="2" onClick={() => this.showModal(null, 2)} className="lexique-icon"></div>
+        <div id="icon4" data-id="3" onClick={() => this.showModal(null, 3)} className="lexique-icon"></div>
+        <div id="icon5" data-id="4" onClick={() => this.showModal(null, 4)} className="lexique-icon"></div>
       </div>
     );
   }
--- src/views/LexiqueModal.js
+++ src/views/LexiqueModal.js
@@ -2,38 +2,31 @@ import React, { Component } from 'react';
 
 
 class LexiqueModal extends Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-          show: true
-      };
-    }
 
-    hideModal = () => {
-        this.setState({
-            show: false
-        });
-    };
+  constructor(props) {
+    super(props)
+  }
 
-    lexiqueItems = () => {
-        var items = this.props.lexiqueObject.map(function(item, index) {
-            return(
-                <div className="row center-xs">
-                    <div key={ index } id="lexique" className="lexique col-xs-4 bg-modal">
-                        <h1>{item.title}</h1>
-                        <p>{item.content}</p>
-                    </div>
-                </div>
-            )
-        });
-        return items
-    }
+  lexiqueItems = () => {
+    return(
+      <div className="row center-xs">
+        <div id="lexique" className="lexique col-xs-10 bg-modal">
+          <h1 className="lexique-title">
+            {this.props.lexique.title}
+          </h1>
+          <p className="lexique-description">
+            {this.props.lexique.content}
+          </p>
+        </div>
+      </div>
+    )
+  }
 
-    render(){
-        return(
-            this.lexiqueItems()
-        )
-    }
+  render(){
+    return(
+      this.lexiqueItems()
+    )
+  }
 }
 
 export default LexiqueModal;
