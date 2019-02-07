ef9c717c27c27f08680d44b8ee3f29f08b6fe5da
Dimitri <Dimitri.vildina@gmail.com>
2019-02-07 11:44:44 +0100

    update conclusion

--- src/views/Conclusion/ConclusionContainer.js
+++ src/views/Conclusion/ConclusionContainer.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Menu from '../Menu/MenuContainer';
 import Skip from '../../assets/Picto/right-arrow-3.svg';
 import '../../assets/style/Conclusion.scss';
+import { TweenLite, TimelineMax, Linear, Back, Sine, Power3 } from 'gsap';
 
 class ConclusionContainer extends Component {
     constructor(props) {
@@ -12,6 +13,17 @@ class ConclusionContainer extends Component {
         }
     }
 
+    componentDidMount(){
+        var elementToAnim = document.querySelectorAll('.intro-anim');
+        console.log(elementToAnim)
+        elementToAnim.forEach((element, i) => {
+            var tl = new TimelineMax({delay:i*7});
+            // tl.fromTo(advanced, 6.6, {scaleX: 0}, {scaleX: 1, transformOrigin:"left", ease: Power3.easeInOut}, 'start')
+            tl.to(element, 1, {autoAlpha:1, ease: Power3.easeInOut}, 'start')
+            tl.to(element, .5, {autoAlpha:0}, '6.6')
+        });
+    }
+
     render() {
         return(
             <section id="Conclusion">
@@ -19,16 +31,22 @@ class ConclusionContainer extends Component {
                     <h1>Capio</h1>
                 </Link>
                 <Menu />
-                <div className="intro1 section">
-                    <p className="intro-anim">
-                    Fondamentalement, les humains ont mené une énorme expérience de la vie réelle en enlevant - et puis en réintroduisant - un prédateur d'une grande étendue de terre. Conséquence, tout l'écosystème de la nature sauvage des États-Unis a été modifié par l'expansion rapide des populations de grands ongulés (wapiti, bison, cerf de Virginie, antilope pronghorn, mouton d'Amérique et la chèvre de montagne). 
-                    </p>
-                    <p className="intro-anim">
-                    La réintroduction des loups gris dans le parc national de Yellowstone figure sûrement, symboliquement et écologiquement, parmi les actes les plus importants de conservation de la faune au XXe siècle. Une fois de plus, Yellowstone abrite toutes les espèces indigènes de grands carnivores: grizzlis, ours noirs, lions de montagne et loups. La présence des loups a même modifié substantiellement les comportements des ongulés. 
-                    </p>
-                    <p className="intro-anim">
-                    Au départ, les changements écologiques provoqués par le manque de loups étaient subtils, de sorte qu'ils n'étaient généralement pas remarqués. Mais les résultats de cette expérience de réintroduction réelle indiquent sans ambiguïté que les loups font partie intégrante de l'écosystème. Ils sont certainement essentiels pour restaurer et maintenir l'écologie naturelle de toute la région de Yellowstone.
-                    </p>
+                <div className="intro1 section grain">
+                    <div className="blockText intro-anim">
+                        <p>
+                        Fondamentalement, les humains ont mené une énorme expérience de la vie réelle en enlevant - et puis en réintroduisant - un prédateur d'une grande étendue de terre. Conséquence, tout l'écosystème de la nature sauvage des États-Unis a été modifié par l'expansion rapide des populations de grands ongulés (wapiti, bison, cerf de Virginie, antilope pronghorn, mouton d'Amérique et la chèvre de montagne). 
+                        </p>
+                    </div>
+                    <div className="blockText intro-anim">
+                        <p>
+                        Fondamentalement, les humains ont mené une énorme expérience de la vie réelle en enlevant - et puis en réintroduisant - un prédateur d'une grande étendue de terre. Conséquence, tout l'écosystème de la nature sauvage des États-Unis a été modifié par l'expansion rapide des populations de grands ongulés (wapiti, bison, cerf de Virginie, antilope pronghorn, mouton d'Amérique et la chèvre de montagne). 
+                        </p>
+                    </div>
+                    <div className="blockText intro-anim">
+                        <p>
+                        Fondamentalement, les humains ont mené une énorme expérience de la vie réelle en enlevant - et puis en réintroduisant - un prédateur d'une grande étendue de terre. Conséquence, tout l'écosystème de la nature sauvage des États-Unis a été modifié par l'expansion rapide des populations de grands ongulés (wapiti, bison, cerf de Virginie, antilope pronghorn, mouton d'Amérique et la chèvre de montagne). 
+                        </p>
+                    </div>
                 </div>
                 <Link to={`/index`} className="Conclusion">
                     <p>Lexique</p>
