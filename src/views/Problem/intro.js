9d7fbe791f755518a649becd1f442a2aee383ab6
metayerromain <romain.metayer@hetic.net>
2019-02-07 21:32:13 +0100

    last changes

--- src/views/Problem/intro.js
+++ src/views/Problem/intro.js
@@ -14,16 +14,25 @@ class Intro1 extends Component {
             <div className="intro1 section">
                 <div className="advanced"></div>
                 <div className="intro-anim">
-                1
+                    L’une des plus fascinantes découvertes scientifiques de ces 50 dernières années a été la découverte de transfert de cascade à grande échelles. 
                 </div>
                 <div className="intro-anim">
-2
+                    Le transfert en cascade d’un processus écologique qui part du sommet de la chaine alimentaire pour se répercuter tout en bas.
                 </div>
                 <div className="intro-anim">
-3
+                    L’un des exemples les plus représentatifs est ce qui s’est produit au parc national de Yellowstone
                 </div>
                 <div className="intro-anim">
-4
+                    En 1995, 14 loups ont été réintroduits dans cet immense espace vert, connu pour son volcan et très apprécié des grimpeurs.
+                </div>
+                <div className="intro-anim">
+                    Avec l’espèce, c’est en fait toute une faune et une flore qui ont été réintroduite. 
+                </div>
+                <div className="intro-anim">
+                    Ce qui a donné aux biologistes une occasion unique d&apos;étudier ce qui se passe lorsqu&apos;un prédateur supérieur retourne à un écosystème.
+                </div>
+                <div className="intro-anim">
+                    Et si, nous aussi, nous faisions l’expérience de retirer le loup puis de le réintroduire au sein de Yellowstone ? 
                 </div>
             </div>
         ) 
