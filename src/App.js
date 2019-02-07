4830f6c1a2c2d9f1087b1e8eb5784f183e52a8e4
Réda Ouaich <reda.ouaich@hetic.net>
2019-02-07 11:09:49 +0100

    Cleaning app component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,37 @@
 import React, { Component, Fragment  } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './assets/style/Reset.scss';
 import HomePage from './views/homePage';
 import AlPage from './views/alPage';
 import SlPage from './views/slPage';
 import IndexPage from './views/indexPage';
 
-// import React, { Component } from 'react';
 
 // //import dependencies
-// import 'flexboxgrid'
-// import "gsap";
+import 'flexboxgrid'
+import "gsap";
 
 // //import css
-// import './assets/style/fonts.scss';
-// import './assets/style/Reset.scss';
-// import './assets/style/content.scss';
-// import './assets/style/grain.scss';
+import './assets/style/fonts.scss';
+import './assets/style/Reset.scss';
+import './assets/style/content.scss';
+import './assets/style/grain.scss';
+import './App.css';
 
 // //import components
 // import Home from './views/Home/HomeContainer';
-// eslint-disable-next-line
 // import Load from './views/Loader/LoaderContainer';
-// import Choice from './views/Choice/ChoiceContainer';
-import Lexique from './views/Lexique/LexiqueContainer';
-// eslint-disable-next-line
-// import Test from './views/Test';
-// import Load from './views/Loader/LoaderContainer';
-// import Choice from './views/Choice/ChoiceContainer';
+import Choice from './views/Choice/ChoiceContainer';
+// import Lexique from './views/Lexique/LexiqueContainer';
 // import Quizz from './views/Quizz/QuizzContainer';
+// import Test from './views/Test';
 
 
-// import './App.css';
 
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    
-    };
-
+    this.state = {};
   }
   render() {
     // console.log("start render images", this.state.images);
@@ -50,7 +41,6 @@ class App extends Component {
     // const { images, contents } = this.state;
     // console.log("after state images",images);
     // console.log("after state content",contents);
-
 
 
     return (
@@ -69,45 +59,22 @@ class App extends Component {
       //       </li>
       //     )}
       //   </ul>
-      // <Load />
-      // <Home />
-      // <Choice />
-      // <Problem/>
-        <Lexique />
       // </div>
-      // <Router>
-      //     <Fragment>
-      //       <Route exact path="/" component={HomePage} />
-      //       <Route path="/choiceAl" component={AlPage} />
-      //       <Route path="/choiceSl" component={SlPage} />
-      //       <Route path="/index" component={IndexPage} />
-      //     </Fragment>
-      //   </Router>
-
-      // <div>
-      //   {/* <Test /> */}
-      //   {/* <ul>
-      //     {images.map(image =>
-      //       <li key={image.id}>
-      //         <img src={image.img}></img>
-      //       </li>
-      //     )}
-      //     {contents.map(content =>
-      //       <li key={content.id}>
-      //       <p>{content.title}</p>
-      //       <p>{content.description}</p>
-      //       </li>
-      //     )}
-      //   </ul> */}
-      //   {/* <Load />
-      //   <Home /> 
-      //   <Choice />
-      //   <Quizz  question={'À ton avis, que chassent les loups ?'}
-      //           goodAnswer={'wapitis'}
-      //           badAnswer={['ours','canards']}
-      //           image={'/images/loup_small_thumbnail.svg'} /> */}
-      //   {/* <Problem/> */}
-      // </div>
+      <Fragment>
+      {/* <Lexique /> */}
+        {/* {/* <Load />
+        <Home /> */}
+        {/* <Choice /> */}
+      {/* <Problem/> */}
+      <Router>
+        <Fragment>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/choiceAl" component={AlPage} />
+          <Route path="/choiceSl" component={SlPage} />
+          <Route path="/index" component={IndexPage} />
+        </Fragment>
+        </Router>
+      </Fragment>
     );
   }
 }
--- src/views/Choice/ChoiceContainer.js
+++ src/views/Choice/ChoiceContainer.js
@@ -3,25 +3,25 @@ import { Link } from 'react-router-dom';
 import '../../assets/style/Choice.scss';
 
 class Choice extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
+  constructor(props) {
+    super(props)
+    this.state = {
 
-        }
     }
+  }
 
-    render() {
-        return(
-            <section id="choices">
-                <Link to={`/choiceAl`} className="buttonAl">
-                    <p>Yellowtone <br /> avec les loups</p>
-                </Link>
-                <Link to={`/choiceSl`} className="buttonSl">
-                    <p>Yellowtone <br /> sans les loups</p>
-                </Link>
-            </section>
-        )
-    }
+  render() {
+      return(
+          <section id="choices">
+              <Link to={`/choiceAl`} className="buttonAl">
+                  <p>Yellowtone <br /> avec les loups</p>
+              </Link>
+              <Link to={`/choiceSl`} className="buttonSl">
+                  <p>Yellowtone <br /> sans les loups</p>
+              </Link>
+          </section>
+      )
+  }
 }
 
 export default Choice;
